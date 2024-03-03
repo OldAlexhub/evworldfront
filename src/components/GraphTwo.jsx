c9894e843efc0e02894e611821cb548feb0495db
OldAlexhub <140672061+OldAlexhub@users.noreply.github.com>
2024-03-03 00:04:24 -0500

    Update GraphTwo.jsx

--- src/components/GraphTwo.jsx
+++ src/components/GraphTwo.jsx
@@ -123,8 +123,8 @@ const GraphTwo = () => {
     >
       <div
         style={{
-          width: "60%", // Adjust width as needed
-          height: "60%", // Adjust height as needed
+          width: "90%", // Adjust width as needed
+          height: "90%", // Adjust height as needed
         }}
       >
         <Line data={chartData} options={chartOptions} />
